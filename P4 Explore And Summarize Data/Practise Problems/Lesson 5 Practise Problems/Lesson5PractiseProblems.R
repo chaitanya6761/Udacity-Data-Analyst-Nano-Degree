c771492ed21487825dddd56812e370d51f630cfe
Chaitanya Kumar Madala <chaitanyakumar943@gmail.com>
2017-07-21 22:45:36 +0530

    adding correlation practise problems

--- P4 Explore And Summarize Data/Practise Problems/Lesson 5 Practise Problems/Lesson5PractiseProblems.R
+++ P4 Explore And Summarize Data/Practise Problems/Lesson 5 Practise Problems/Lesson5PractiseProblems.R
@@ -55,5 +55,73 @@ ggplot(aes(x=age,y=friend_count_mean),data = pf.fc_by_age) +
 pf.s <- ggplot(aes(x=age,y=friend_count),data=pf)+
   xlim(13,90)+
   geom_point(alpha=0.05,position=position_jitter(h=0),color='orange')+
-  coord_trans(y='sqrt')+
-  geom_line(stat='summary',fun.y=mean)
+  coord_trans(y='sqrt')
+
+## Adding Mean geom Line
+
+pf.m <- pf.s + geom_line(stat='summary',fun.y=mean)
+pf.m
+
+## Adding 10% and 90% quantile
+
+pf.q <- pf.m + geom_line(stat = 'summary',fun.y=quantile,fun.args = list(probs = .9),linetype=2,color='blue')
+pf.q
+
+## Adding median the 50% mark line
+
+pf.med <- pf.q + geom_line(stat = 'summary',fun.y='quantile',fun.args = list(probs=.5),color='blue')
+pf.med
+
+
+## Correlation 
+
+cor.test(pf$age,pf$friend_count)
+
+with(pf,cor.test(age,friend_count))
+
+
+## Correlation on subsets
+
+with(subset(pf,age<=70),cor.test(age,friend_count))
+
+
+## create a scatter Plot between likes_received (y) vs. www_likes_received (x).
+
+ggplot(aes(www_likes_received,likes_received),data = pf)+
+  geom_point()
+
+## Create a scatter plot for above graph with 95% quantile
+ggplot(aes(www_likes_received,likes_received),data = pf)+
+  geom_point()+
+  xlim(0,quantile(pf$www_likes_received,0.95))+
+  ylim(0,quantile(pf$likes_received,0.95))+
+  geom_smooth(method='lm',color='red')
+
+with(pf,cor.test(www_likes_received,likes_received))
+
+
+##Caution with correlation
+install.packages('alr3')
+library(alr3)
+
+data("Mitchell")
+names(Mitchell)
+
+# Create a scatterplot of temperature (Temp) vs. months (Month).
+
+ggplot(aes(x=Month,y=Temp),data=Mitchell)+
+  geom_point()
+
+with(Mitchell,cor.test(Temp,Month))
+
+ggplot(aes(x=Month,y=Temp),data=Mitchell)+
+  geom_point()+
+  scale_x_continuous(breaks = seq(0,204,12))
+
+
+
+
+
+
+
+
