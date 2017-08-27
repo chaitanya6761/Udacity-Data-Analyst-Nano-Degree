664a1cfd589c4ec9fabc30b291f02d15047c630f
Chaitanya Kumar Madala <chaitanyakumar943@gmail.com>
2017-08-27 09:48:52 +0530

    Adding Lesson 7 Practice Problems

--- P4 Explore And Summarize Data/Practise Problems/Lesson 7 Practise Problems/Lesson7PractiseProblems.R
+++ P4 Explore And Summarize Data/Practise Problems/Lesson 7 Practise Problems/Lesson7PractiseProblems.R
@@ -34,14 +34,56 @@ pf.fc_by_age_gender <- arrange(pf.fc_by_age_gender,age,gender)
 # Create a line graph showing the median friend count over the ages for each gender. 
 #Be sure to use the data frame you just created,pf.fc_by_age_gender.
 
-ggplot(aes(x=age,y=median_friend_count),data=pf.fc_by_age_gender)+
+ggplot(aes(x=age,y=mean_friend_count),data=pf.fc_by_age_gender)+
   geom_line(aes(color=gender))
 
 
+# Reshaping the data
+install.packages('tidyr')
+library(tidyr)
+
+pf.fc_by_age_gender.wide <- spread(subset(pf.fc_by_age_gender,
+                                          select=c(age,gender,median_friend_count)),
+                                   gender,median_friend_count)
+
+pf.fc_by_age_gender.wide
 
 
 
 
+pf.fc_by_age_gender.wide <-
+  subset(pf.fc_by_age_gender[c('age', 'gender', 'median_friend_count')],
+         !is.na(gender)) %>%
+  spread(gender, median_friend_count) %>%
+  mutate(ratio = male / female)
+
+head(pf.fc_by_age_gender.wide)
+
+# Plot the ratio of the female to male median friend counts using the 
+#data frame pf.fc_by_age_gender.wide.
+ggplot(aes(x=age,y=female/male),data=pf.fc_by_age_gender.wide)+
+  geom_line()+
+  geom_hline(yintercept = 1,color='red',linetype=2,size=1)
 
 
+# Create a variable called year_joined in the pf data frame using the variable
+# tenure and 2014 as the reference year.
+
+pf$year_joined <- floor(2014 - (pf$tenure/365))
+
+table(pf$year_joined)
+
+# Create a new variable in the data frame called year_joined.bucket by using 
+#the cut function on the variable year_joined.
+
+pf$year_joined_bucket <- cut(pf$year_joined,c(2004,2009,2011,2012,2014))
+
+table(pf$year_joined_bucket)
  
+
+ggplot(aes(x = age, y = friend_count),
+       data = subset(pf, !is.na(year_joined_bucket))) +
+  geom_line(aes(color = year_joined_bucket), stat = 'summary', fun.y = mean)+
+  geom_line(stat='summary',fun.y=mean,linetype=2)
+
+
