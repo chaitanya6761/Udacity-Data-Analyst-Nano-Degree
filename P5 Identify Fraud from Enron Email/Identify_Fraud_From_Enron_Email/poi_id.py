a1b1cf983eb62f9b301010a0b81756444922f1fd
Chaitanya Kumar Madala <chaitanyakumar943@gmail.com>
2018-09-11 08:47:02 +0530

    adding naive bayes

--- P5 Identify Fraud from Enron Email/Identify_Fraud_From_Enron_Email/poi_id.py
+++ P5 Identify Fraud from Enron Email/Identify_Fraud_From_Enron_Email/poi_id.py
@@ -6,11 +6,34 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from sklearn.cross_validation import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import SelectKBest,f_classif
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','salary','to_messages',
+ 'deferral_payments',
+ 'total_payments',
+ 'exercised_stock_options',
+ 'bonus',
+ 'restricted_stock',
+ 'shared_receipt_with_poi',
+ 'restricted_stock_deferred',
+ 'total_stock_value',
+ 'expenses',
+ 'loan_advances',
+ 'from_messages',
+ 'other',
+ 'from_this_person_to_poi',
+ 'director_fees',
+ 'deferred_income',
+ 'long_term_incentive',
+ 'from_poi_to_this_person'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -24,6 +47,13 @@ my_dataset = data_dict
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+selector = SelectKBest(f_classif, k=5)
+selector.fit(features_train,labels_train)
+features_train_transformed = selector.transform(features_train)
+features_test_transformed = selector.transform(features_test)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -32,12 +62,6 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.cross_validation import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import accuracy_score
-
 clf = GaussianNB()
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
@@ -48,11 +72,8 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
-
-clf.fit(features_train,labels_train)
-pred = clf.predict(features_test)
+clf.fit(features_train_transformed,labels_train)
+pred = clf.predict(features_test_transformed)
 print "Total number of people in test set:", len(labels_test)
 print "How many poi's are there in the testset: ",sum(labels_test)
 
