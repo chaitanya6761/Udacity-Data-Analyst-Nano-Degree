d082813619dd6ebbb28f197440512b71428f7dcb
Chaitanya Kumar Madala <chaitanyakumar943@gmail.com>
2018-10-17 09:40:41 +0530

    updating naive bayes

--- P5 Identify Fraud from Enron Email/Practise Problems/naive_bayes/nb_author_id.py
+++ P5 Identify Fraud from Enron Email/Practise Problems/naive_bayes/nb_author_id.py
@@ -21,16 +21,29 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB,MultinomialNB
 
 classifier = GaussianNB()
 t0 = time()
 classifier.fit(features_train,labels_train)
-print "Training Time:",(time()-t0)
+print "Training Time Of GaussianNB :",(time()-t0)
 
 t1 = time()
 classifier.predict(features_test)
-print "Predicting Time",(time() - t1)
+print "Predicting Time Of GaussianNB",(time() - t1)
 
-print "Accuracy",classifier.score(features_test,labels_test)
+print "Accuracy Of GaussianNB",classifier.score(features_test,labels_test)
 
+print"---------------------------------------------------------------------"
+
+
+classifier = MultinomialNB()
+t0 = time()
+classifier.fit(features_train,labels_train)
+print "Training Time Of MultinomialNB :",(time()-t0)
+
+t1 = time()
+classifier.predict(features_test)
+print "Predicting Time Of MultinomialNB",(time() - t1)
+
+print "Accuracy Of MultinomialNB",classifier.score(features_test,labels_test)
